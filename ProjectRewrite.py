d8a2efbdd5a489e9d48957e1923dacef179ddc84
CsabaPorcellan <70797040+CsabaPorcellan@users.noreply.github.com>
2020-09-21 18:58:55 +0200

    Add files via upload

---
+++ ProjectRewrite.py
@@ -0,0 +1,30 @@
+min(x, y) 
+    For any two real numbers the less is returned
+    No built-in functions are used
+
+max(values_list)
+
+    For any list of real numbers the greatest is returned
+    No built-in functions are used
+
+Implement the len(values_list) function for a list of numbers or strings which returns the length of the list!
+
+    For any list the count of the elements is returned
+    No built-in functions are used
+
+Implement the multiply(x, y) function for integer numbers as inputs! Do not use the *, /, and // operators and any built-in functions, but you may (and should) use +.
+
+    For any two integer inputs the returned value equals the result of x * y
+    Neither *, /, // nor any built-in functions are used
+mplement the pow(x, y) function for real base numbers and positive integer exponents! Do not use the ** operator and any built-in functions! Here you can use *.
+
+    For inputs from the specified domain the returned value equals the result of x**y
+    Neither ** nor any built-in functions are used
+
+divmod()
+OPTIONAL Implement the divmod(x, y) function for for two integer numbers as inputs! Do not use the // and the % operators and any built-in functions! It should return a tuple: the first value should be equal to the value of x // y and the second equal to the value of x % y! Do not use the // operator and any built-in functions!
+
+    For any two positive integer inputs the returned value equals (x // y, x % y)
+    For any two +/- integer inputs the returned value equals (x // y, x % y)
+    Neither // nor any built-in functions are used
+
